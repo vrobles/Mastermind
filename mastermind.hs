b6b3b15d2d41eb5b729e759f9e3c29d18e55359b
vanessa robles <vanessita.robles@gmail.com>
2013-02-05 17:22:05 -0500

    origin

--- mastermind.hs
+++ mastermind.hs
@@ -1,9 +1,22 @@
 import Data.Char
 
+
 main::IO()
+
 main = do
 	putStr "Ingrese nombre de archivo de la clave (ej: archivo.txt):"
 	fentrada <- getLine
 	cad <- readFile fentrada
-	let digito1 = cad !! 1
-	print cad
+	y<- intList cad
+
+	
+	print y
+
+
+
+
+intList :: String -> IO [Int]
+intList = readIO
+
+
+
